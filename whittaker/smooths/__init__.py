b116911a9d5fefcdd83f0b407d532d7d71a2f26b
Richard Iannone <riannone@me.com>
2026-07-20 00:57:27 -0400

    Update __init__.py

--- whittaker/smooths/__init__.py
+++ whittaker/smooths/__init__.py
@@ -4,10 +4,12 @@ from __future__ import annotations
 
 from whittaker.smooths.base import SmoothBasis
 from whittaker.smooths.cubic import CRS
+from whittaker.smooths.pspline import PSpline
 from whittaker.smooths.tprs import TPRS
 
 __all__ = [
     "CRS",
+    "PSpline",
     "SmoothBasis",
     "TPRS",
 ]
