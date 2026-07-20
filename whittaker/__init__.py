7cc2594fd0e2bce585b4c86a642ae12d29ea1839
Richard Iannone <riannone@me.com>
2026-07-19 23:55:23 -0400

    Export formula API from package init

--- whittaker/__init__.py
+++ whittaker/__init__.py
@@ -4,9 +4,20 @@ from __future__ import annotations
 
 from importlib.metadata import PackageNotFoundError, version
 
+from whittaker.formula import Formula, InteractionTerm, LinearTerm, OffsetTerm, SmoothTerm
+from whittaker.formula import parse as parse_formula
+
 try:
     __version__: str = version("whittaker")
 except PackageNotFoundError:
     __version__ = "0.0.0+unknown"
 
-__all__ = ["__version__"]
+__all__ = [
+    "__version__",
+    "Formula",
+    "InteractionTerm",
+    "LinearTerm",
+    "OffsetTerm",
+    "SmoothTerm",
+    "parse_formula",
+]
