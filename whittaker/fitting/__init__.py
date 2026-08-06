f4b33577bb7cf937d428ff322691e20bd31d3daa
Richard Iannone <riannone@me.com>
2026-08-06 11:10:36 -0400

    Update __init__.py

--- whittaker/fitting/__init__.py
+++ whittaker/fitting/__init__.py
@@ -2,9 +2,12 @@
 
 from __future__ import annotations
 
+from whittaker.fitting.inference import SmoothTestResult, smooth_tests
 from whittaker.fitting.pirls import FitResult, pirls_fit
 
 __all__ = [
     "FitResult",
+    "SmoothTestResult",
     "pirls_fit",
+    "smooth_tests",
 ]
