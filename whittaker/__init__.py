053e94d204dbf5d6befd24af67316f9b7f5e6e3b
Richard Iannone <riannone@me.com>
2026-08-09 23:34:42 -0400

    Update __init__.py

--- whittaker/__init__.py
+++ whittaker/__init__.py
@@ -4,6 +4,8 @@ from __future__ import annotations
 
 from importlib.metadata import PackageNotFoundError, version
 
+from whittaker.calibration import calibrate_sigma
+from whittaker.cross_validation import CVResult, cross_validate
 from whittaker.families import Binomial, Family, Gamma, Gaussian, NegativeBinomial, Poisson
 from whittaker.formula import Formula, InteractionTerm, LinearTerm, OffsetTerm, SmoothTerm
 from whittaker.formula import parse as parse_formula
@@ -18,6 +20,9 @@ except PackageNotFoundError:
 
 __all__ = [
     "__version__",
+    "calibrate_sigma",
+    "cross_validate",
+    "CVResult",
     "Binomial",
     "CRS",
     "Family",
