84cbbea03cd15975cd3a5eb362e2ed14a2794d3c
Richard Iannone <riannone@me.com>
2026-08-10 16:33:40 -0400

    Update __init__.py

--- whittaker/__init__.py
+++ whittaker/__init__.py
@@ -7,13 +7,30 @@ from importlib.metadata import PackageNotFoundError, version
 from whittaker.bam import BigGAM
 from whittaker.calibration import calibrate_sigma
 from whittaker.cross_validation import CVResult, cross_validate
-from whittaker.families import Beta, Binomial, Family, Gamma, Gaussian, NegativeBinomial, Poisson
+from whittaker.families import (
+    Beta,
+    Binomial,
+    Family,
+    Gamma,
+    Gaussian,
+    NegativeBinomial,
+    Poisson,
+    tw,
+)
 from whittaker.formula import Formula, InteractionTerm, LinearTerm, OffsetTerm, SmoothTerm
 from whittaker.formula import parse as parse_formula
 from whittaker.gam import GAM
 from whittaker.model_matrix import ModelMatrix, build_model_matrix, predict_matrix
 from whittaker.sklearn import GAMClassifier, GAMRegressor
-from whittaker.smooths import CRS, TPRS, GaussianProcess, PSpline, SmoothBasis, SoapFilm
+from whittaker.smooths import (
+    CRS,
+    TPRS,
+    DuchonSpline,
+    GaussianProcess,
+    PSpline,
+    SmoothBasis,
+    SoapFilm,
+)
 
 try:
     __version__: str = version("whittaker")
@@ -29,6 +46,7 @@ __all__ = [
     "Beta",
     "Binomial",
     "CRS",
+    "DuchonSpline",
     "Family",
     "Formula",
     "GAM",
@@ -48,6 +66,7 @@ __all__ = [
     "SmoothTerm",
     "SoapFilm",
     "TPRS",
+    "tw",
     "build_model_matrix",
     "parse_formula",
     "predict_matrix",
