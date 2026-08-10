3a00ab0b5bd503ee04c8d888b4ea1113eee39478
Richard Iannone <riannone@me.com>
2026-08-09 21:26:45 -0400

    Update __init__.py

--- whittaker/families/__init__.py
+++ whittaker/families/__init__.py
@@ -4,18 +4,26 @@ from __future__ import annotations
 
 from whittaker.families.base import Family
 from whittaker.families.binomial import Binomial
+from whittaker.families.gamlss_base import GAMLSSFamily
 from whittaker.families.gamma import Gamma
 from whittaker.families.gaussian import Gaussian
+from whittaker.families.gaussian_ls import GaussianLS
+from whittaker.families.inverse_gaussian import InverseGaussian
 from whittaker.families.negative_binomial import NegativeBinomial
 from whittaker.families.poisson import Poisson
+from whittaker.families.quantile import QuantileFamily
 from whittaker.families.tweedie import Tweedie
 
 __all__ = [
     "Binomial",
     "Family",
+    "GAMLSSFamily",
     "Gamma",
     "Gaussian",
+    "GaussianLS",
+    "InverseGaussian",
     "NegativeBinomial",
     "Poisson",
+    "QuantileFamily",
     "Tweedie",
 ]
