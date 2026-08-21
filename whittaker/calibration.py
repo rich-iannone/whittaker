50849b50382944257d95ead5a67f10d70fadc412
Richard Iannone <riannone@me.com>
2026-08-21 16:48:37 -0400

    Update calibration.py

--- whittaker/calibration.py
+++ whittaker/calibration.py
@@ -58,7 +58,7 @@ def calibrate_sigma(
     Returns
     -------
     float
-        Calibrated sigma value (the one minimising CV pinball loss).
+        Calibrated sigma value (the one minimizing CV pinball loss).
 
     Examples
     --------
